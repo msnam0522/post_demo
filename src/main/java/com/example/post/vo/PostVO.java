d083bfd4b4a55f7078cfb57787b1976c1534d853
bearscho <43170264+bearscho@users.noreply.github.com>
2019-06-20 17:03:29 +0900

    Upload new files

---
+++ src/main/java/com/example/post/vo/PostVO.java
@@ -0,0 +1,12 @@
+package com.example.post.vo;
+
+import lombok.Data;
+
+@Data
+public class PostVO {
+	private Integer no;
+	private String name;
+	private String pwd;
+	private String content;
+	private String regDate;
+}
